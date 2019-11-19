d1248d7fc44fac5eca4c305837bc21a66d6fcc1d
dd_minaev <dd_minaev@bingo-boom.com>
2019-11-19 17:06:34 +0300

    process env fix

--- src/mixins/api.js
+++ src/mixins/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.API_URL || "http://localhost:3002/api";
+const API_=URL = process.env.VUE_APP_API_URL || "http://localhost:3002/api";
 const axios = require("axios");
 export const requestsMixin = {
     methods: {
